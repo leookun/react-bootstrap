6ef5434a8e1a1064d213d5a83d478fdcc88cd726
leokun <leokun.cn@gmail.com>
2026-02-02 20:47:25 +0800

    Remove ESLint configuration and update project files
    - Deleted eslint.config.js as it is no longer needed.
    - Changed the title in index.html to "APP NAME".
    - Updated package.json scripts for development and linting.
    - Added new dependencies including Tailwind CSS and code inspector plugins.
    - Enhanced vite.config.ts with new plugins and configurations.
    - Updated App component with new UI elements and login buttons.
    - Imported Tailwind CSS in main.tsx for styling.

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,4 +3,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   react: true,
   typescript: true,
+  ignores: ['imports.d.ts', 'auto-import-components.d.ts', 'vite.config.ts'],
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,44 @@
+/* eslint-disable antfu/top-level-function */
+import type { UserConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
+import autoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
-// https://vite.dev/config/
+const isDev = process.env.NODE_ENV === 'development'
+
+const removeDebuggerPlugin = () => ({
+  name: 'remove-debugger',
+  config(config: UserConfig, { command }: { command: 'build' | 'serve' }) {
+    if (command === 'build') {
+      config.build = config.build || {}
+      config.build.minify = 'terser'
+      config.build.terserOptions = {
+        compress: {
+          drop_debugger: true,
+          drop_console: false,
+        },
+      } as any
+    }
+  },
+})
+
 export default defineConfig({
   plugins: [
-    react({
-      babel: {},
+    isDev && codeInspectorPlugin({
+      bundler: 'vite',
+      editor: 'cursor',
+      hotKeys: ['ctrlKey'],
     }),
-  ],
+    removeDebuggerPlugin(),
+    topLevelAwait(),
+    react(),
+    autoImport({
+      dts: 'imports.d.ts',
+      imports: ['react', 'react-router-dom'],
+    }),
+    tailwindcss(),
+  ].filter(Boolean),
 })
